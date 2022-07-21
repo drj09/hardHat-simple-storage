e360f6091d5e74884d5c871194ae806545cf4dad
Dheeraj <Dheeraj160199@gmail.com>
2022-07-21 20:35:55 +0530

    changes harhat configuration for adding alchemy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+      chainId: 4,
+    },
+  },
   solidity: "0.8.9",
 };
